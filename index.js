74def1fae916ecf0102b9b451402baa59f1e835d
Jelena Feliciano <jfel217@gmail.com>
2021-04-14 15:46:37 -0400

    rendered the index template added profile image and updated some details

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
+app.set('view engine', 'pug');
+
 //routing to the index 
 app.get('/', (req, res) => {
-    res.send('This is the start of the profile');
+    res.render('index');
 });
 
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('This app is running on localhost: 3000!');
+});
