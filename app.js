5752e907b5399d6414a90c640efb3a7a9d22411e
Jelena Feliciano <jfel217@gmail.com>
2021-04-30 09:52:53 -0400

    changed file name

--- app.js
+++ app.js
@@ -21,14 +21,14 @@ app.get('/about', (req,res) => {
 app.use((req, res, next) => {
     console.log('404 error handler called');
 
-    res.status(404).render('pageNotFound');
+    res.status(404).render('page-not-found');
 })
 //error handler to catch global errors
 app.use((err, req, res, next) => {
     res.locals.error = err; 
     if(err.status === 404) {
         err.message = `Oops! Looks like the project you're looking for is not found 😥`;
-        res.status(404).render('pageNotFound', {err});
+        res.status(404).render('page-not-found', {err});
     } else {
         err.message = `Oops! Looks like something went wrong with the server 😥`;
         res.status(err.status || 500).render('error', {err});
